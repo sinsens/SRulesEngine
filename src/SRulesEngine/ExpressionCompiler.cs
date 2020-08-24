5cb9c216516b4e590ebe55f9bd69d8d1d18308d7
Sinsen <1278386874@qq.com>
2020-08-24 10:54:25 +0800

    表达式编译器增加可选启动参数配置支持

--- src/SRulesEngine/ExpressionCompiler.cs
+++ src/SRulesEngine/ExpressionCompiler.cs
@@ -6,6 +6,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.Caching;
 using System.Text;
@@ -19,9 +20,16 @@ namespace SRulesEngine
         private static readonly MemoryCache _expressionCache = new MemoryCache($"SRulesEngine_{Guid.NewGuid()}");
 
         /// <summary>
-        /// 缓存持续时间：分钟
+        /// 是否开启缓存
         /// </summary>
-        private static readonly int _expressionCacheAlive = 5;
+        private static readonly string enableCache = "disabled";
+
+        /// <summary>
+        /// 缓存持续时间：秒
+        /// </summary>
+        private static readonly int CacheAlive = 60;
+
+        private static bool EnableCache { get => enableCache != "disabled"; }
 
 #endif
 
@@ -30,7 +38,63 @@ namespace SRulesEngine
         /// </summary>
         public static readonly char MarkChar = '#';
 
+        // 本来打算用正则提取， 奈何功力不够，提取不出支持复杂公式的表达式
         //private static readonly Regex _paramReg = new Regex("(\\$\\(.*?\\))", RegexOptions.Compiled);
+
+        /// <summary>
+        /// 配置文件名称
+        /// </summary>
+        private static readonly string _configfilename;
+
+        static ExpressionCompiler()
+        {
+            _configfilename = "Compiler.ini";
+            // 读取配置，如果存在的话
+            if (File.Exists(_configfilename))
+            {
+                var configLines = File.ReadAllLines(_configfilename);
+                var configDict = new Dictionary<string, string>();
+                foreach (var item in configLines)
+                {
+                    var configline = item.Trim();
+                    if (string.IsNullOrWhiteSpace(configline) || configline.StartsWith("#"))
+                    {
+                        continue;
+                    }
+                    var config = configline.Split('=');
+                    if (config.Length < 2)
+                    {
+                        continue;
+                    }
+                    configDict.Add(config[0].Trim(), config[1].Trim());
+                }
+                if (configDict.Any())
+                {
+                    foreach (var item in configDict)
+                    {
+                        var mb = typeof(ExpressionCompiler).GetField(item.Key, System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.IgnoreCase | System.Reflection.BindingFlags.NonPublic);
+                        if (mb != null)
+                        {
+                            try
+                            {
+                                object value = item.Value;
+                                if (int.TryParse(item.Value, out int result))
+                                {
+                                    mb.SetValue(new ExpressionCompiler(), result);
+                                }
+                                else
+                                {
+                                    mb.SetValue(new ExpressionCompiler(), value);
+                                }
+                            }
+                            catch
+                            {
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
         public string Compile(string expr, IDictionary<string, object> input)
         {
@@ -44,11 +108,15 @@ namespace SRulesEngine
             }
 
 #if ENABLE_CACHE
-            // 判断内存缓存
             var cacheKeyName = GetCacheKey(expr, input);
-            if (_expressionCache.Contains(cacheKeyName))
+
+            if (EnableCache && CacheAlive > 0)
             {
-                return _expressionCache.Get(cacheKeyName) as string;
+                // 判断内存缓存
+                if (_expressionCache.Contains(cacheKeyName))
+                {
+                    return _expressionCache.Get(cacheKeyName) as string;
+                }
             }
 #endif
 
@@ -75,7 +143,7 @@ namespace SRulesEngine
             }
             */
 
-            // JS 表达式(代码)截取、替换
+            // JS 表达式(代码)截取、替换，这里性能不够好，得优化
             var tempStr = tempBuilder.ToString();
             int startIndex = tempStr.IndexOf(MarkChar);
             int nextIndex = tempStr.IndexOf(MarkChar, startIndex + 1);
@@ -93,7 +161,10 @@ namespace SRulesEngine
             }
 
 #if ENABLE_CACHE
-            _expressionCache.AddOrGetExisting(cacheKeyName, tempBuilder.ToString(), DateTime.Now.AddMinutes(_expressionCacheAlive));
+            if (EnableCache && CacheAlive > 0)
+            {
+                _expressionCache.AddOrGetExisting(cacheKeyName, tempBuilder.ToString(), DateTime.Now.AddSeconds(CacheAlive));
+            }
 #endif
             // 清理标记字符
             tempBuilder.Replace(MarkChar.ToString(), string.Empty);
